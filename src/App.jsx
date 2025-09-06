3a10b37a4570e9de747e5dc8cfbaba46358db94a
Aditya Shah <aditya546shah@gmail.com>
2025-09-06 09:53:31 +0530

    Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import EcommercePlatform from './Components/Ecommerce';
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <EcommercePlatform/>
+      </div>
+    </>
+  )
+}
+
+export default App
